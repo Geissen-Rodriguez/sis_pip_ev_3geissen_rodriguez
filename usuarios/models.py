516ca8d3e91d711b148ff17107cfe3c156401900
Geissen-Rodriguez <geissen.rodriguez@gmail.com>
2025-11-12 13:02:16 -0300

    creacion de  modelo

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    CARGOS_VALIDOS = [
+        ('medico', 'Medico'),
+        ('matron', 'Matron/a'),
+        ('enfermero', 'Enfermero/a'),
+        ('admin', 'Administrativo'),
+    ]
+    username = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=20, choices=CARGOS_VALIDOS)
+    intentos_fallidos = models.IntegerField(default=0)
+    bloqueado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.username} ({self.cargo})"
+
+class CodigoRecuperacion(models.Model):
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    codigo = models.CharField(max_length=6)
+    creado = models.DateTimeField(auto_now_add=True)
